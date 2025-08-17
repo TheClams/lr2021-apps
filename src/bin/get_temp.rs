58422b143682674949de268bee5ae44f7ab238d5
TheClams <clams@mail.com>
2025-08-17 07:09:58 +0200

    [Bin] GetTemp: now report temperature on the UART (Virtual COM)

--- src/bin/get_temp.rs
+++ src/bin/get_temp.rs
@@ -4,11 +4,20 @@
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::{gpio::{Input, Level, Output, Pull, Speed}, time::Hertz};
-use embassy_stm32::spi::{Config, Spi};
+use embassy_stm32::spi::{Config as SpiConfig, Spi};
+use embassy_stm32::usart::{Config as UartConfig, Uart};
+use embassy_stm32::{bind_interrupts, peripherals, usart};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
+use core::fmt::Write;
+use heapless::String;
+
 use lr2021::{system::{self, AdcRes, TempSrc}, Lr2021};
+
+bind_interrupts!(struct Irqs {
+    USART2 => usart::InterruptHandler<peripherals::USART2>;
+});
 
 /// Task to blink up to two leds
 #[embassy_executor::task(pool_size = 2)]
@@ -22,25 +31,10 @@ async fn blink(mut led: Output<'static>, delay: u64) {
     }
 }
 
-
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    info!("Starting get_temp (v3)");
-
-    // Pin mapping
-    // Name  | Connector | Nucleo
-    // NRST  | CN8 A0    | PA0
-    // SCK   | CN5 D13   | PA5
-    // MISO  | CN5 D12   | PA6
-    // MOSI  | CN5 D11   | PA7
-    // NSS   | CN9 D7    | PA8
-    // BUSY  | CN9 D3    | PB3
-    // IRQ   | CN9 D2    | PA10
-    // RFSW0 | CN9 D0    | PA3
-    // RXSW1 | CN9 D1    | PA2
-    // LEDTX | CN8 A5    | PC0
-    // LEDRX | CN8 A4    | PC1
+    info!("Starting get_temp (v4)");
 
     // Blink both TX/RX LEDs on the radio module to confirm PIN mapping
     let led_tx = Output::new(p.PC1, Level::High, Speed::Low);
@@ -54,19 +48,30 @@ async fn main(spawner: Spawner) {
     let nreset = Output::new(p.PA0, Level::High, Speed::Low);
 
     // SPI
-    let mut spi_config = Config::default();
+    let mut spi_config = SpiConfig::default();
     spi_config.frequency = Hertz(4_000_000);
     let spi = Spi::new(p.SPI1, p.PA5, p.PA7, p.PA6, p.DMA1_CH3, p.DMA1_CH2, spi_config);
     let nss = Output::new(p.PA8, Level::High, Speed::VeryHigh);
+
+    // UART
+    let uart_config = UartConfig::default();
+    let mut uart = Uart::new(p.USART2, p.PA3, p.PA2, Irqs, p.DMA1_CH7, p.DMA1_CH6, uart_config).unwrap();
 
     let mut lr2021 = Lr2021::new_blocking(nreset, busy, spi, nss);
     lr2021.reset().await
         .unwrap_or_else(|_| error!("Unable to reset chip !"));
 
+    // String buffer
+    let mut s: String<128> = String::new();
+
     // Check version
     let mut fw_version = system::VersionRsp::new();
     match lr2021.cmd_rd(&system::get_version_req(), fw_version.as_mut()).await {
-        Ok(_) => info!("FW Version {:02x}.{:02x}", fw_version.major(), fw_version.minor()),
+        Ok(_) => {
+            info!("FW Version {:02x}.{:02x}", fw_version.major(), fw_version.minor());
+            core::write!(&mut s, "FW Version {}.{}!\r\n", fw_version.major(), fw_version.minor()).unwrap();
+            uart.write(s.as_bytes()).await.ok();
+        }
         Err(e) => error!("{}", e),
     }
 
@@ -79,12 +84,20 @@ async fn main(spawner: Spawner) {
     // Create the GetTemp command once
     let cmd = system::get_temp_req(TempSrc::Vbe, AdcRes::Res13bit);
 
-    // Get a temperature measurement every 15 seconds
+    // Get periodic temperature measurement
     loop {
-        Timer::after_secs(15).await;
+        Timer::after_secs(10).await;
         let mut temp = system::TempRsp::new();
         match lr2021.cmd_rd(&cmd, temp.as_mut()).await {
-            Ok(_) => info!("Temp = {}", temp),
+            Ok(_) => {
+                info!("Temp = {}", temp);
+                s.clear();
+                match core::write!(&mut s, "T = {}\r\n", temp.temp_celsius()) {
+                    Ok(_) => {uart.write(s.as_bytes()).await.ok();}
+                    Err(_) => {error!("Unable to write string");}
+                }
+
+            }
             Err(e) => error!("{}", e),
         }
     }
